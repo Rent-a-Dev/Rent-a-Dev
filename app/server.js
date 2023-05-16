fddda350d1fef09bc16c4647f454edeab8f13b19
StevenPintoVC <steven.pinto@vcontractor.co.za>
2023-05-16 11:14:44 +0200

    html and css mostly done for search and filter. Functionality is pending.

--- app/server.js
+++ app/server.js
@@ -4,6 +4,8 @@ const port = 3000;
     
 app.set('view engine', 'ejs'); 
  
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get("/viewRequests", function(req, res) {  
     res.render("pages/viewRequests", {data: [{id: 0, name: 'James', teamLead: 'Anne', requestStatus: 'Pending', hours: 5, startDate: '2023/05/01', endDate: '2023/05/03'}, 
                                             {id: 1,name: 'James', teamLead: 'Anne', requestStatus: 'Approved', hours: 5, startDate: '2023/05/01', endDate: '2023/05/03'}, 
