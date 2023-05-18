fc2b042d757d6a7c5f3a42ca6a172b617fa5daa4
Nicholas Joannou <nicholasj@bbd.co.za>
2023-05-18 10:57:40 +0200

    - Fixed all db functions - Created function to return all info related to a developer - converted all files from ES modules to CommonJS

--- backend/db/dbConfig.js
+++ backend/db/dbConfig.js
@@ -1,2 +1,6 @@
-import sqlite3 from 'sqlite3';
-export let db = new sqlite3.Database('./backend/db/rent-a-dev');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./backend/db/rent-a-dev');
+
+module.exports = {
+  db,
+};
