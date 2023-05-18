930683df12d199556cbb55de5b925d07e49b99bb
Jared-Swanzen <jared@bbd.co.za>
2023-05-18 09:10:00 +0200

    fixed the broken stuffs

--- app/public/js/requestTable.js
+++ app/public/js/requestTable.js
@@ -1,33 +1,14 @@
-function showHideAvailability(id) {
-  let row = document.getElementById(id);
-  if (row.classList.contains('hiddenRow')) {
-    row.classList.remove('hiddenRow');
-    row.classList.add('visibleRow');
-  }
-  else {
-    row.classList.remove('visibleRow');
-    row.classList.add('hiddenRow');
-  }
-}
-
-function configurePage() {
-  let isApproveRequestsPage = '<%=isApproveRequests%>';
-  let columns = document.getElementsByClassName('availabilityColumn');
-  let columnName = document.getElementById("teamLead");
-
-  if (isApproveRequestsPage) {
-    columnName.innerHTML = 'Requested by';
-    for (const column of columns) {
-      column.classList.add('visibleColumn');
-    }
-  }
-  else {
-    columnName.innerHTML = 'Team Lead';
-    for (const column of columns) {
-      column.classList.add('hiddenColumn');
-    }
-  }
-}
+// function showHideAvailability(id) {
+//   let row = document.getElementById(id);
+//   if (row.classList.contains('hiddenRow')) {
+//     row.classList.remove('hiddenRow');
+//     row.classList.add('visibleRow');
+//   }
+//   else {
+//     row.classList.remove('visibleRow');
+//     row.classList.add('hiddenRow');
+//   }
+// }
 
 function approveRequest(id) {
   console.log("approved request with id " + id);
@@ -36,5 +17,3 @@ function approveRequest(id) {
 function rejectRequest(id) {
   console.log("rejected request with id " + id);
 }
-
-configurePage();
